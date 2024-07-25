4f6adb034f384f8a9f6e97d2771d272024be9f76
KWJ5668 <tocando12@gmail.com>
2024-07-25 17:27:21 +0900

    20240725 kwj

--- src/main/frontend/src/component/MainCommunityList.js
+++ src/main/frontend/src/component/MainCommunityList.js
@@ -4,7 +4,7 @@ import "swiper/css";
 import "./MainCommunityList.css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, /*Autoplay*/ } from "swiper/modules";
 
 export default function App() {
   return (
@@ -14,51 +14,11 @@ export default function App() {
         spaceBetween={10}
         navigation={true}
         loop={true}
-        modules={[Pagination, Navigation]}
+        // speed={3000}
+        // autoplay={{delay:0, disableOnInteraction:false}}
+        modules={[Pagination, Navigation, /*Autoplay*/]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
-          <p>
-            게시글 내용이 노출됩니다.게시글 내용이 노출됩니다.게시글 내용이
-            노출됩니다.게시글 내용이 노출됩니다.
-          </p>
-        </SwiperSlide>
         <SwiperSlide>
           <h5>게시글 타이틀이 두 줄까지 노출됩니다</h5>
           <p>
